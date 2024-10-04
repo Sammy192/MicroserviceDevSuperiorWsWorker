f87cbc9427be6b81b1207a58c210f70783330e0b
Samuel R Borges <sammy_rb@hotmail.com>
2024-10-04 11:18:27 -0300

    Configuration of Docker files

--- hr-eureka-server/src/test/java/com/devsuperior/hreurekaserver/HrEurekaServerApplicationTests.java
+++ hr-eureka-server/src/test/java/com/devsuperior/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -3,7 +3,7 @@ package com.devsuperior.hreurekaserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = { HrEurekaServerApplicationTests.class })
 class HrEurekaServerApplicationTests {
 
 	@Test
